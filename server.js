4b8c3e4274d7ab8a14bb0d4b7b225fef0130d763
Peter Wilmot <pwilmot@Peters-MacBook-Pro.local>
2016-02-18 13:56:55 -0800

    updating paths

--- server.js
+++ server.js
@@ -6,24 +6,24 @@ var cors = require('cors')
 app.use(cors());
 app.use('/static', express.static('public'));
 
-app.get('/odorblocker', function(req, res) {
+app.get('/ob', function(req, res) {
     res.sendFile(path.join(__dirname + '/odorblocker.html'));
 });
 
-app.get('/dirtdestroyer', function(req, res) {
+app.get('/dd', function(req, res) {
     res.sendFile(path.join(__dirname + '/dirtdestroyer.html'));
 });
 
-app.get('/frame/odorblocker', function(req, res) {
-	res.end('<iframe src="./odorblocker" style="border:none"></iframe>')
+app.get('/odorblocker', function(req, res) {
+	res.end('<iframe src="./ob" style="border:none"></iframe>')
 })
 
-app.get('/frame/dirtdestroyer', function(req, res) {
-	res.end('<iframe src="./dirtdestroyer" style="border:none"></iframe>')
+app.get('/dirtdestroyer', function(req, res) {
+	res.end('<iframe src="./dd" style="border:none"></iframe>')
 })
 
 app.get('/test', function(req, res) {
-	res.end('<iframe src="./dirtdestroyer" style="border:none"></iframe>')
+	res.end('<iframe src="./dd" style="border:none"></iframe>')
 })
 
 app.listen(process.env.PORT || 8080);
