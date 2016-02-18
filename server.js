f09fb6d47dfca8771eb0a96c48a2909ff22a8fb9
Peter Wilmot <pwilmot@Peters-MacBook-Pro.local>
2016-02-18 13:52:48 -0800

    adding cors support

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var cors = require('cors')
 
-// viewed at http://localhost:8080
-
+app.use(cors());
 app.use('/static', express.static('public'));
 
 app.get('/odorblocker', function(req, res) {
