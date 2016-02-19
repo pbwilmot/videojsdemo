81fce2f7ea0a006c1793dbf78140bab92047a91a
Peter Wilmot <pwilmot@Peters-MacBook-Pro.local>
2016-02-19 12:15:18 -0800

    adding trackers to oldspice ads at start and completion

--- public/javascript/dirtdestroyer.js
+++ public/javascript/dirtdestroyer.js
@@ -21,9 +21,7 @@ var options = {
   adTagUrl: 'https://svastx.moatads.com/buzzstartervpaid67711111384/Buzzstarter128413474.xml'
 };
 
-player.ima(options);
-
-this.events = [
+var events = [
   google.ima.AdEvent.Type.ALL_ADS_COMPLETED,
   google.ima.AdEvent.Type.CLICK,
   google.ima.AdEvent.Type.COMPLETE,
@@ -35,12 +33,56 @@ this.events = [
   google.ima.AdEvent.Type.THIRD_QUARTILE
 ];
 
-for (var index = 0; index < this.events.length; index++) {
-  this.player.ima.addEventListener(
-      this.events[index],
-      this.bind(this, this.onAdEvent));
+var bind = function(thisObj, fn) {
+  return function() {
+    fn.apply(thisObj, arguments);
+  };
+};
+
+var adsManagerLoadedCallback = function() {
+  log('adsManagerLoadedCallback')
+  for (var index = 0; index < events.length; index++) {
+    player.ima.addEventListener(
+        events[index],
+        bind(this, onAdEvent));
+  }
+  player.ima.start();
+};
+
+player.ima(
+      options,
+      bind(this, adsManagerLoadedCallback));
+
+var addTracker = function(path) {
+  log('addTracker: ' + path)
+  var img = new Image();
+  var div = document.getElementById('content_video');
+
+  img.onload = function() {
+    div.appendChild(img);
+  };
+
+  img.src = path;
 }
-  
+
+var onAdEvent = function(event) {
+  this.log('Ad event: ' + event.type);
+  switch(event.type) {
+    case google.ima.AdEvent.Type.STARTED:
+      log('AdStarted')
+      addTracker('//clickserv.sitescout.com/conv/1b6b1eff2c522caf');
+    break;
+    case   google.ima.AdEvent.Type.COMPLETE:
+      log('AdCompleted')
+      addTracker('//clickserv.sitescout.com/conv/3a5d71631ff1e5df');
+    break;
+  }
+};
+
+var log = function(message) {
+  // console.log(message);
+}
+
 // Remove controls from the player on iPad to stop native controls from stealing
 // our click
 var contentPlayer =  document.getElementById('content_video_html5_api');
--- public/javascript/odorblocker.js
+++ public/javascript/odorblocker.js
@@ -21,8 +21,67 @@ var options = {
   adTagUrl: 'https://svastx.moatads.com/buzzstartervpaid67711111384/Buzzstarter_128048632.xml'
 };
 
-player.ima(options);
+var events = [
+  google.ima.AdEvent.Type.ALL_ADS_COMPLETED,
+  google.ima.AdEvent.Type.CLICK,
+  google.ima.AdEvent.Type.COMPLETE,
+  google.ima.AdEvent.Type.FIRST_QUARTILE,
+  google.ima.AdEvent.Type.LOADED,
+  google.ima.AdEvent.Type.MIDPOINT,
+  google.ima.AdEvent.Type.PAUSED,
+  google.ima.AdEvent.Type.STARTED,
+  google.ima.AdEvent.Type.THIRD_QUARTILE
+];
 
+var bind = function(thisObj, fn) {
+  return function() {
+    fn.apply(thisObj, arguments);
+  };
+};
+
+var adsManagerLoadedCallback = function() {
+  log('adsManagerLoadedCallback')
+  for (var index = 0; index < events.length; index++) {
+    player.ima.addEventListener(
+        events[index],
+        bind(this, onAdEvent));
+  }
+  player.ima.start();
+};
+
+player.ima(
+      options,
+      bind(this, adsManagerLoadedCallback));
+
+var addTracker = function(path) {
+  log('addTracker: ' + path)
+  var img = new Image();
+  var div = document.getElementById('content_video');
+
+  img.onload = function() {
+    div.appendChild(img);
+  };
+
+  img.src = path;
+}
+
+var onAdEvent = function(event) {
+  this.log('Ad event: ' + event.type);
+  switch(event.type) {
+    case google.ima.AdEvent.Type.STARTED:
+      log('AdStarted')
+      addTracker('//clickserv.sitescout.com/conv/87df0625460bc347');
+    break;
+    case   google.ima.AdEvent.Type.COMPLETE:
+      log('AdCompleted')
+      addTracker('//clickserv.sitescout.com/conv/d5fada484628dfd2');
+    break;
+  }
+};
+
+var log = function(message) {
+  console.log(message);
+}
 // Remove controls from the player on iPad to stop native controls from stealing
 // our click
 var contentPlayer =  document.getElementById('content_video_html5_api');
