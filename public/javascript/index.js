a580c64f97c8e2b8b58449c8bdf781bacff5081b
Nicholas Pappagalo <npappagalo@gmail.com>
2016-02-19 14:29:38 -0800

    add close iframe button

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,17 +1,22 @@
-var show = document.getElementById('example-show');
-var hide = document.getElementById('example-hide');
+var show = document.getElementById('slider-show');
+var hide = document.getElementById('slider-hide');
+var closeFrame = document.getElementById('close-iframe');
 
 show.addEventListener('click', function(){
-  showHide('example');
+  showHide('slider');
 });
 
 hide.addEventListener('click', function(){
-  showHide('example');
+  showHide('slider');
+});
+
+closeFrame.addEventListener('click', function() {
+  showHide('slider');
 });
 
 function showHide(shID) {
   if (document.getElementById(shID)) {
-    if (document.getElementById('example').style.height === '0px') {
+    if (document.getElementById('slider').style.height === '0px') {
       document.getElementById(shID+'-show').style.display = 'none';
       document.getElementById(shID+'-hide').style.display = 'inline';
       document.getElementById(shID).style.height = '364px';
