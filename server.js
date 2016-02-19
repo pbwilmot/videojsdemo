9528008424035ef79edf8e76f6a6b5d345f3acd3
Nicholas Pappagalo <npappagalo@gmail.com>
2016-02-18 16:16:47 -0800

    remove extra iframe

--- server.js
+++ server.js
@@ -6,24 +6,12 @@ var cors = require('cors')
 app.use(cors());
 app.use('/static', express.static('public'));
 
-app.get('/ob', function(req, res) {
-    res.sendFile(path.join(__dirname + '/odorblocker.html'));
-});
-
-app.get('/dd', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dirtdestroyer.html'));
-});
-
 app.get('/odorblocker', function(req, res) {
-	res.end('<iframe src="./ob" style="border:none"></iframe>')
-})
+	res.sendFile(path.join(__dirname + '/odorblocker.html'));
+});
 
 app.get('/dirtdestroyer', function(req, res) {
-	res.end('<iframe src="./dd" style="border:none"></iframe>')
-})
-
-app.get('/test', function(req, res) {
-	res.end('<iframe src="./dd" style="border:none"></iframe>')
-})
+	res.sendFile(path.join(__dirname + '/dirtdestroyer.html'));
+});
 
 app.listen(process.env.PORT || 8080);
