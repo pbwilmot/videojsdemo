0621c992c24abe68ee9cb158e73419d8a987997c
Nicholas Pappagalo <npappagalo@gmail.com>
2016-02-22 21:19:20 -0800

    mute / unmute progress

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,8 @@ var closeFrame = document.getElementById('close-iframe');
 var toggleSlider = document.getElementById('toggle-slider');
 var slider = document.getElementById('slider');
 var closeBtn = document.getElementById('toggle-close');
+var contentVideo = document.getElementById('content_video');
+var adMuted = false;
 
 toggleSlider.addEventListener('click', function(){
   showHide();
@@ -18,6 +20,34 @@ closeBtn.addEventListener('click', function() {
 window.addEventListener('scroll', function() {
   startInView();
 });
+
+// contentVideo.addEventListener('mouseover', function() {
+//   adsManager.setVolume(0);
+// });
+
+adsManager = adsManagerLoadedEvent.getAdsManager(
+    contentPlayheadTracker, adsRenderingSettings);
+
+document.getElementById('ima-sample-videoplayer').addEventListener('mouseover', function() {
+  console.log('hi');
+  var muteDiv = document.getElementById('ima-mute-div');
+  if (adMuted) {
+    muteDiv.className = 'ima-non-muted';
+    adsManager.setVolume(1);
+    // Bubble down to content player
+    player.muted(false);
+    adMuted = false;
+    sliderLevelDiv.style.width = player.volume() * 100 + "%";
+  } else {
+    muteDiv.className = 'ima-muted';
+    adsManager.setVolume(0);
+    // Bubble down to content player
+    player.muted(true);
+    adMuted = true;
+    sliderLevelDiv.style.width = "0%";
+  }
+});
+
 
 function showHide() {
   if (slider) {
