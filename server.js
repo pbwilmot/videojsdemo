973dc56c25620539718df74d16fd1e4a4d7a935e
jasonchangliu <jason@buzzstarter.com>
2016-02-23 11:11:07 -0800

    remove unused html test pages, clean up routes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var cors = require('cors')
 app.set('view engine', 'jade');
 
 app.use(cors());
-app.use('/static', express.static('public'));
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -14,26 +13,6 @@ app.get('/', function(req, res) {
 
 app.get('/jade', function(req, res) {
   res.render('video', { tag: req.query.tag, autoplay: req.query.autoplay, poster: req.query.poster});
-});
-
-app.get('/odorblocker', function(req, res) {
-	res.sendFile(path.join(__dirname + '/odorblocker.html'));
-});
-
-app.get('/dirtdestroyer', function(req, res) {
-	res.sendFile(path.join(__dirname + '/dirtdestroyer.html'));
-});
-
-app.get('/vine', function(req, res) {
-	res.sendFile(path.join(__dirname + '/vine.html'));
-});
-
-app.get('/twitch', function(req, res) {
-	res.sendFile(path.join(__dirname + '/twitch.html'));
-});
-
-app.get('/youtube', function(req, res) {
-	res.sendFile(path.join(__dirname + '/youtube.html'));
 });
 
 app.listen(process.env.PORT || 8080);
