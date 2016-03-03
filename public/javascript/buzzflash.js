f73090a0282677ece18e4441190f26df8c26affd
Khurt <khurtious@gmail.com>
2016-03-03 13:18:46 -0800

    multi-bcod temporary

--- public/javascript/buzzflash.js
+++ public/javascript/buzzflash.js
@@ -1,7 +1,60 @@
 var BuzzFlash = (function(window) {
+  const dictionary = {
+    "82WxOWdT7ZRR": [
+    {bcod: "JlJnbNZiNkQ74", weight: 1},
+    {bcod: "eL9EJn7hAMvGx", weight: 2},
+    {bcod: "qR99qJaTmkLB", weight: 2},
+    {bcod: "L71PZgetmWmkj", weight: 2},
+    {bcod: "gbQaVX0t2qq2", weight: 2},
+    {bcod: "mwJdroRfyBL01", weight: 1},
+    {bcod: "Vnv3GryU5EZ4Q", weight: 1},
+    {bcod: "EdWeyPLcNZAgN", weight: 1}
+    ],
+    "M21bX1qTgRLaW": [
+    {bcod: "QZQAbwMHbwEJ0", weight: 1},
+    {bcod: "4vg7AyGSL0l3x", weight: 2},
+    {bcod: "DXMMeLZhMW48G", weight: 2},
+    {bcod: "dDdxBQWcGjPdZ", weight: 2},
+    {bcod: "ybkN1ratyeLnM", weight: 2},
+    {bcod: "LkD1Q9EU2BlQg", weight: 1},
+    {bcod: "dJNAwg4UGbeAW", weight: 1},
+    {bcod: "rkwmg45SJdMao", weight: 1}
+    ],
+    "X7Av9AjfdEr5J": [
+    {bcod: "Anm5bp7SDmDyl", weight: 1},
+    {bcod: "PpLvwZDHGEXDQ", weight: 2},
+    {bcod: "WoNNnV8TM9pDW", weight: 2},
+    {bcod: "apdx2QjiVmaq8", weight: 2},
+    {bcod: "3ewPDjqfpM08k", weight: 2},
+    {bcod: "0B2O3DnHkRlVW", weight: 1},
+    {bcod: "08yVvR3tlD3w", weight: 1},
+    {bcod: "Z5wEWLMTj3Xj3", weight: 1}
+    ]
+  };
+
+  function replacebcod(bcod){
+
+    if (!dictionary[bcod]){
+      return bcod;
+    }
+
+    var = arr = dictionary[bcod];
+    var rn = Math.floor(Math.random()*12);
+    var c = 0;
+    for(i = 0; i < arr.length; i++){
+      c += arr[i].weight;
+      if(rn < c){
+        return arr[i].bcod;
+      }
+    }
+    return bcod;
+  }
 
   var options, tracking, source, completionWindow, autoplay;
   var initPlayer = function (domtarget, bcod){
+
+    bcod = replacebcod(bcod);
+
     options = QueryStringToJSON();
     tracking = (options.tracking || 'xhr');
     source = (options.src || '//buzz.st/v1/ads/'+bcod+'/vast');
